f25400574b50e95297589eb46aa32eb62b9cef20
vickydeveloper <vickyroman404@gmail.com>
2024-01-07 18:18:31 +0530

    fm

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public void run() {
+        for(int i=0; i<5; i++){
+
+            System.out.println("Child Thread");
+        }
+    }public void runa() {
+        for(int i=0; i<5; i++){
+
+            System.out.println("Child Thread");
+        }
+    }
+}
