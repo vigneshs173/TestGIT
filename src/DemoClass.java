587345563450b9b7b5a006ca513edf3f370992e2
vickydeveloper <vickyroman404@gmail.com>
2024-01-14 21:17:09 +0530

    added new thread

--- src/DemoClass.java
+++ src/DemoClass.java
@@ -7,7 +7,7 @@ public class DemoClass extends Thread{
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         DemoClass t1 = new DemoClass();
         Thread t2 = new Thread(()->{
@@ -15,9 +15,18 @@ public class DemoClass extends Thread{
                 System.out.println("Thread 2");
         });
 
+        Thread t3 = new Thread(()->{
+            for(int i=0; i<10; i++)
+                System.out.println("Thread 3");
+        });
 
         t1.start();
         t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
 
         for(int i=0; i<10; i++) {
             System.out.println("Main Thread");
